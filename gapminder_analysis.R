220589227d93c0e5ededfa40c59ccae9357ac8e4
Nadine Kotlarz <kotlarz@umich.edu>
2017-01-18 11:43:25 -0500

    Created figure with x = year and y = life_exp per continent

--- gapminder_analysis.R
+++ gapminder_analysis.R
@@ -6,5 +6,9 @@ download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/
 gapminder <- read.csv("gapminder-FiveYearData.csv")
 head(gapminder)
 tail(gapminder)
-
 colnames(gapminder)
+#create plot with year vs life expectancy
+ggplot(data=gapminder, aes(x=year, y=lifeExp, color=continent)) + geom_point() + facet_grid(.~continent) #shows a panel for each continent (can do this for each mycobacterium culture)
+
+#save plot, defaults to save the last plot made
+ggsave(filename="year_vs_lifexp_percont.png", width = 5, height = 4, units= "in")
