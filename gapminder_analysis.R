edec7468c08d68d72d514fe551934301cb55208f
Nadine Kotlarz <kotlarz@umich.edu>
2017-01-18 10:42:15 -0500

    Adding gapminder_analysis.R to repository

---
+++ gapminder_analysis.R
@@ -0,0 +1,6 @@
+download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv", destfile = "gapminder-FiveYearData.csv")
+gapminder <- read.csv("gapminder-FiveYearData.csv")
+head(gapminder)
+tail(gapminder)
+
+colnames(gapminder)
