8438417082cbbb70655c6c40df9aac95ab46524d
Nadine Kotlarz <kotlarz@umich.edu>
2017-01-18 11:09:07 -0500

    Added header notes

--- gapminder_analysis.R
+++ gapminder_analysis.R
@@ -1,3 +1,6 @@
+#Notes from software carpentry workshop
+#Date January 17-18,2017
+
 download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv", destfile = "gapminder-FiveYearData.csv")
 gapminder <- read.csv("gapminder-FiveYearData.csv")
 head(gapminder)
